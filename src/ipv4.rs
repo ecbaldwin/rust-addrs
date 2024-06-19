3ac5669c95cbbf8b5a3b6ad31143213cc831a9ac
Carl Baldwin <carl@ecbaldwin.net>
2024-06-19 14:40:44 -0600

    begin documentation

--- src/ipv4.rs
+++ src/ipv4.rs
@@ -1,15 +1,35 @@
+use std::net::Ipv4Addr;
+
+/// Defines minimum requirements of an ipv4 address for this crate
+///
+/// The purpose of this trait is not to replace nor even add to [`Ipv4Addr`]. It is well thought
+/// out and (nearly) complete enough for my purposes[^1]. For this reason, this crate doesn't even
+/// provide a type alias for it.
+///
+/// This trait is mostly for use within this crate to formalize its good properties on which this
+/// crate may depend. We also provide a minimal set of integration tests which are meant as a
+/// sanity check on the its behavior, not a comprehensive test suite.
+///
+/// This trait also servers to limit the touch points this crate has on that type.
+///
+/// [^1]: One minor exception is that I wish it implemented Into<[u8; 4]>. There is
+/// [`Ipv4Addr::octets`] but I cannot make that a formal requirement of this trait.
 pub trait Address:
     Eq
     + From<u32>
     + Into<u32>
     + From<[u8; 4]>
-    // I'm not sure how to handle this. Ipv4 has `octets` which I use implicitly
-    // + Into<[u8; 4]>
     + std::string::ToString
     + std::str::FromStr
-    + Copy + Clone + Send + Sized + Sync + Unpin
+    + Copy
+    + Clone
+    + Send
+    + Sized
+    + Sync
+    + Unpin
 {
+    /// formalize that all v4 address are 32 bits
     const BITS: u8 = 32;
 }
 
-impl Address for std::net::Ipv4Addr {}
+impl Address for Ipv4Addr {}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,18 @@
+//! Provides IP address related types and data structures for the Rust programming language with a
+//! clean and complete API and many nice features. The basic types are opaque, immutable,
+//! comparable, space efficient, and defined as simple structs that don't require extra memory
+//! allocation.
+//!
+//! First, a little history: I write a lot of go code in my job and I do a lot of virtual
+//! networking. I work with IP Addresses regularly. I was unhappy with the implementation of IP
+//! addresses [in the go standard library][gonet], and a few other go libraries, for a number of
+//! reasons, so [I wrote my own][goaddrs].
+//!
+//! For now, programming in Rust is more a part of my hobby-life than my work-life but I wanted to
+//! replicate the functionality of my go library in Rust for two reasons: because I think it could
+//! enrich Rust and I want a good learning experience.
+//!
+//! [goaddrs]: https://pkg.go.dev/gopkg.in/addrs.v1
+//! [gonet]: https://pkg.go.dev/net#IP
+
 pub mod ipv4;
