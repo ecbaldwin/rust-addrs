397664c56345743af33141fb5a1009800fbb690d
Carl Baldwin <carl@ecbaldwin.net>
2024-06-19 13:53:29 -0600

    reorganize tests using intergration style

--- src/ipv4.rs
+++ src/ipv4.rs
@@ -1,5 +1,3 @@
-use std::net::Ipv4Addr;
-
 pub trait Address:
     Eq
     + From<u32>
@@ -14,66 +12,4 @@ pub trait Address:
     const BITS: u8 = 32;
 }
 
-impl Address for Ipv4Addr {}
-
-#[cfg(test)]
-mod tests {
-    type AddressImpl = Ipv4Addr;
-
-    use super::*;
-
-    fn _ipv4_addr(s: &str) -> AddressImpl {
-        s.parse().expect("bad ip")
-    }
-
-    fn address_compare(a: &str, b: &str, eq: bool) {
-        fn compare<A: Address>(a: A, b: A, eq: bool) {
-            assert_eq!(eq, a == b);
-            assert_ne!(eq, a != b);
-        }
-        compare(_ipv4_addr(a), _ipv4_addr(b), eq)
-    }
-
-    crate::tests! { address_compare {
-        equal("10.0.0.1", "10.0.0.1", true);
-        not_equal("10.0.0.1", "10.0.0.2", false);
-        zero_eq("0.0.0.0", "0.0.0.0", true);
-        all_ones_eq("255.255.255.255", "255.255.255.255", true);
-        extrames("0.0.0.0", "255.255.255.255", false);
-    } }
-
-    #[test]
-    fn address_size() {
-        assert_eq!(32u8, <AddressImpl as Address>::BITS);
-    }
-
-    #[test]
-    fn address_from_string() {
-        let ip: AddressImpl = "10.224.24.1".parse().expect("bad ip");
-        assert_eq!(AddressImpl::from(0x0ae01801), ip);
-    }
-
-    #[test]
-    fn address_from_bytes() {
-        let ip: AddressImpl = [10, 224, 24, 1].into();
-        assert_eq!(AddressImpl::from(0x0ae01801u32), ip);
-    }
-
-    #[test]
-    fn address_to_u32() {
-        let ip: AddressImpl = [10, 224, 24, 1].into();
-        assert_eq!(0x0ae01801u32, ip.into());
-    }
-
-    #[test]
-    fn address_to_string() {
-        let ip: AddressImpl = [10, 224, 24, 1].into();
-        assert_eq!("10.224.24.1", ip.to_string());
-    }
-
-    #[test]
-    fn address_to_octets() {
-        let ip: AddressImpl = [10, 224, 24, 1].into();
-        assert_eq!([10, 224, 24, 1], ip.octets());
-    }
-}
+impl Address for std::net::Ipv4Addr {}
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,1 @@
 pub mod ipv4;
-
-#[macro_export]
-macro_rules! tests {
-    ($property_test_func:ident {
-        $( $(#[$attr:meta])* $test_name:ident( $( $param:expr ),* ); )+
-    }) => {
-        $(
-            paste::paste! {
-                $(#[$attr])*
-                #[test]
-                fn [<$property_test_func ˬˬ $test_name>]() {
-                    $property_test_func($( $param ),* )
-                }
-            }
-        )+
-    }
-}
